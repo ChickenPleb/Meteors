d2edc316239b7535407d8d32bdba9e99e5ac7397
PigBelli <42427714+PigBelli@users.noreply.github.com>
2018-08-17 16:04:42 -0700

    Umm new cool additions

--- level1.lua
+++ level1.lua
@@ -1,4 +1,4 @@
------------------------------------------------------------------------------------------
+x-----------------------------------------------------------------------------------------
 --
 -- level1.lua
 --
@@ -18,7 +18,7 @@ local screenW, screenH, halfW = display.actualContentWidth, display.actualConten
 function scene:create( event )
 
 	-- Called when the scene's view does not exist.
-	-- 
+	--
 	-- INSERT code here to initialize the scene
 	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
 
@@ -37,33 +37,33 @@ function scene:create( event )
 	local background = display.newImageRect( "crate.jpg", display.actualContentWidth, display.actualContentHeight )
 	background.anchorX = 0
 	background.anchorY = 0
-	background.x = 0 + display.screenOriginX 
+	background.x = 0 + display.screenOriginX
 	background.y = 0 + display.screenOriginY
-	
+
 	-- make a crate (off-screen), position it, and rotate slightly
 	local crate = display.newImageRect( "crate.jpg", 90, 90 )
 	crate.x, crate.y = 160, -100
 	crate.rotation = 15
-	
+
 	-- add physics to the crate
 	physics.addBody( crate, { density=1.0, friction=0.3, bounce=0.3 } )
-	
+
 	-- create a grass object and add physics (with custom shape)
 	local bricks = display.newImageRect( "bluec.jpg", 1050,250) --, screenW, 82 )
 	bricks.anchorX = 0
 	bricks.anchorY = 1
 	--  draw the grass at the very bottom of the screen
 	bricks.x, bricks.y = display.screenOriginX, display.actualContentHeight + display.screenOriginY
-	
+
 	-- define a shape that's slightly shorter than image bounds (set draw mode to "hybrid" or "debug" to see)
 	--local bricksShape = { -halfW,-34, halfW,-34, halfW,34, -halfW,34 }
 	physics.addBody( bricks, "static", { friction=0.3} )
-  
+
   local company = display.newText ( "Infiny",display.contentCenterX,400,  "motel_king.ttf", 80)
 	company:setFillColor(1,1,1)
   company.alpha=0
   transition.to(company, {alpha=1, time=2000})
-  
+
   local ball = display.newImageRect( "8ball.png", 250,250)
   ball.x = 500
 	ball.y = 225
@@ -71,7 +71,7 @@ function scene:create( event )
   ball.alpha=0
   transition.to (ball, {rotation=55, time=2000, alpha=1})
   --ball:rotate(45)
-	
+
 	-- all display objects must be inserted into group
 	sceneGroup:insert( background )
 	sceneGroup:insert( bricks)
@@ -84,12 +84,12 @@ end
 function scene:show( event )
 	local sceneGroup = self.view
 	local phase = event.phase
-	
+
 	if phase == "will" then
 		-- Called when the scene is still off screen and is about to move on screen
 	elseif phase == "did" then
 		-- Called when the scene is now on screen
-		-- 
+		--
 		-- INSERT code here to make the scene come alive
 		-- e.g. start timers, begin animation, play audio, etc.
 		physics.start()
@@ -98,9 +98,9 @@ end
 
 function scene:hide( event )
 	local sceneGroup = self.view
-	
+
 	local phase = event.phase
-	
+
 	if event.phase == "will" then
 		-- Called when the scene is on screen and is about to move off screen
 		--
@@ -109,18 +109,18 @@ function scene:hide( event )
 		physics.stop()
 	elseif phase == "did" then
 		-- Called when the scene is now off screen
-	end	
-	
+	end
+
 end
 
 function scene:destroy( event )
 
 	-- Called prior to the removal of scene's "view" (sceneGroup)
-	-- 
+	--
 	-- INSERT code here to cleanup the scene
 	-- e.g. remove display objects, remove touch listeners, save state, etc.
 	local sceneGroup = self.view
-	
+
 	package.loaded[physics] = nil
 	physics = nil
 end
